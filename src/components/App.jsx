4bd21a07798a7ba1946e2017e14962052f74db6d
Vikram S <60956383+vikramoff24@users.noreply.github.com>
2020-09-12 23:27:28 +0530

    Added App Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,28 @@
 import React, { useState } from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>
+        Hello
+        <br />
+        {fullName.fName} {fullName.lName}
+        <br />
+      </h1>
+
+      <input
+        name="fName"
+        placeHolder="First Name"
+        value={fullName.fName}
+        onChange={handleChange}
+      />
+      <input
+        name="lName"
+        placeHolder="Last Name"
+        mvalue={fullName.lName}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </div>
+  );
+}
